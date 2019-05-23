e7f44bc65b3be7748e070f99455e3164cc1708e7
Igor Matyushkin <igormatyushkin014@gmail.com>
2019-05-24 02:11:14 +0300

    Implemented ButtonManager class

--- vootstrap.js
+++ vootstrap.js
@@ -1,12 +1,82 @@
-Vue.component("vt-button", {
-	data: function() {
-		return {
-		};
-	},
-	props: [
-		"title"
+/*
+	Buttons.
+*/
+
+class ButtonManager {
+
+	getCssClasses(
+		appearance,
+		outline,
+		size
+	) {
+		let classes = [
+			"btn",
+			`btn-${appearance}${ outline ? "-outline" : "" }`,
+			`${ size ? size : "" }`
+		];
+		return classes.join(" ");
+	}
+
+	register(
+		appearance,
+		outline,
+		size
+	) {
+		let name = `vt-button-${appearance}`;
+		let cssClasses = this.getCssClasses(
+			appearance,
+			outline,
+			size
+		);
+		Vue.component(name, {
+			data: function() {
+				return {
+				};
+			},
+			props: [
+				"title"
+			],
+			template: `
+				<button
+					type="button"
+					class="${cssClasses}"
+				>
+					{{ title }}
+				</button>
+			`
+		});
+	}
+}
+
+let buttonCssClasses = {
+	appearance: [
+		"primary",
+		"secondary",
+		"success",
+		"danger",
+		"warning",
+		"info",
+		"light",
+		"dark",
+		"link"
 	],
-	template: `
-		<button type="button" class="btn btn-primary">{{ title }}</button>
-	`
-});
+	outline: [
+		true,
+		false
+	],
+	size: [
+		"btn-lg",
+		"btn-sm"
+	]
+};
+
+let buttonManager = new ButtonManager();
+buttonManager.register("primary");
+buttonManager.register("secondary");
+buttonManager.register("success");
+buttonManager.register("danger");
+buttonManager.register("warning");
+buttonManager.register("info");
+buttonManager.register("light");
+buttonManager.register("dark");
+buttonManager.register("link");
