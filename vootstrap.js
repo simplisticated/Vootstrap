42d97245c43435f4bc7f769acf4782997f0ae56a
Igor Matyushkin <igormatyushkin014@gmail.com>
2019-05-24 12:45:04 +0300

    Implemented all combinations of style

--- vootstrap.js
+++ vootstrap.js
@@ -2,81 +2,180 @@
 	Buttons.
 */
 
-class ButtonManager {
+const buttonStylePrimary = "primary";
+const buttonStyleSecondary = "secondary";
+const buttonStyleSuccess = "success";
+const buttonStyleDanger = "danger";
+const buttonStyleWarning = "warning";
+const buttonStyleInfo = "info";
+const buttonStyleLight = "light";
+const buttonStyleDark = "dark";
+const buttonStyleLink = "link";
 
-	getCssClasses(
-		appearance,
-		outline,
-		size
-	) {
-		let classes = [
-			"btn",
-			`btn-${appearance}${ outline ? "-outline" : "" }`,
-			`${ size ? size : "" }`
+const buttonSizeSmall = "small";
+const buttonSizeLarge = "large";
+
+class ButtonGenerator {
+
+	getStyleCssClass(style, outline) {
+		return `btn${ outline ? "-outline" : "" }-${style}`;
+	}
+
+	getSizeCssClass(size) {
+		switch (size) {
+			case buttonSizeSmall: {
+				return "btn-sm";
+			}
+			case buttonSizeLarge: {
+				return "btn-lg";
+			}
+			default: {
+				return "";
+			}
+		}
+	}
+
+	/*
+		Example of `appearance` object:
+
+		let appearance = {
+			style: buttonStylePrimary,
+			outline: false,
+			size: buttonSizeLarge
+		}
+	*/
+	getCssClasses(appearance) {
+		var classes = [
+			"btn"
 		];
+
+		if (appearance.style) {
+			classes.push(
+				this.getStyleCssClass(
+					appearance.style,
+					appearance.outline
+				)
+			);
+		}
+
+		if (appearance.size) {
+			classes.push(
+				this.getSizeCssClass(appearance.size)
+			);
+		}
+
 		return classes.join(" ");
 	}
 
-	register(
-		appearance,
-		outline,
-		size
-	) {
-		let name = `vt-button-${appearance}`;
-		let cssClasses = this.getCssClasses(
-			appearance,
-			outline,
-			size
+	getName(appearance) {
+		return `vt-button-${appearance.style}`;
+	}
+
+	getPossibleNames(appearance) {
+		let nameComponents = {
+			style: appearance.style ? "-" + appearance.style : "",
+			outline: appearance.outline ? "-outline" : "",
+			size: appearance.size ? "-" + appearance.size : ""
+		};
+
+		var names = [];
+		names.push(
+			`vt-button${nameComponents.style}${nameComponents.outline}${nameComponents.size}`
 		);
-		Vue.component(name, {
-			data: function() {
-				return {
-				};
-			},
-			props: [
-				"title"
-			],
-			template: `
-				<button
-					type="button"
-					class="${cssClasses}"
-				>
-					{{ title }}
-				</button>
-			`
-		});
+		names.push(
+			`vt-button${nameComponents.style}${nameComponents.size}${nameComponents.outline}`
+		);
+		names.push(
+			`vt-button${nameComponents.outline}${nameComponents.style}${nameComponents.size}`
+		);
+		names.push(
+			`vt-button${nameComponents.outline}${nameComponents.size}${nameComponents.style}`
+		);
+		names.push(
+			`vt-button${nameComponents.size}${nameComponents.style}${nameComponents.outline}`
+		);
+		names.push(
+			`vt-button${nameComponents.size}${nameComponents.outline}${nameComponents.style}`
+		);
+		return names;
+	}
+
+	register(appearance) {
+		let names = this.getPossibleNames(appearance);
+		let cssClasses = this.getCssClasses(appearance);
+		
+		for (var name of names) {
+			Vue.component(name, {
+				data: function() {
+					return {
+					};
+				},
+				props: [
+					"title"
+				],
+				template: `
+					<button
+						type="button"
+						class="${cssClasses}"
+					>
+						{{ title }}
+					</button>
+				`
+			});
+		}
+	}
+
+	getPossibleAppearances() {
+		let styles = [
+			buttonStylePrimary,
+			buttonStyleSecondary,
+			buttonStylePrimary,
+			buttonStyleSecondary,
+			buttonStyleSuccess,
+			buttonStyleDanger,
+			buttonStyleWarning,
+			buttonStyleInfo,
+			buttonStyleLight,
+			buttonStyleDark,
+			buttonStyleLink,
+			null
+		];
+
+		let outlines = [
+			true,
+			false
+		];
+
+		let sizes = [
+			buttonSizeLarge,
+			buttonSizeSmall,
+			null
+		];
+
+		var appearances = [];
+
+		for (var style of styles) {
+			for (var outline of outlines) {
+				for (var size of sizes) {
+					appearances.push({
+						style: style,
+						outline: outline,
+						size: size
+					});
+				}
+			}
+		}
+
+		return appearances;
+	}
+
+	registerAllAppearances() {
+		let allAppearances = this.getPossibleAppearances();
+
+		for (var appearance of allAppearances) {
+			this.register(appearance);
+		}
 	}
 }
 
-let buttonCssClasses = {
-	appearance: [
-		"primary",
-		"secondary",
-		"success",
-		"danger",
-		"warning",
-		"info",
-		"light",
-		"dark",
-		"link"
-	],
-	outline: [
-		true,
-		false
-	],
-	size: [
-		"btn-lg",
-		"btn-sm"
-	]
-};
-
-let buttonManager = new ButtonManager();
-buttonManager.register("primary");
-buttonManager.register("secondary");
-buttonManager.register("success");
-buttonManager.register("danger");
-buttonManager.register("warning");
-buttonManager.register("info");
-buttonManager.register("light");
-buttonManager.register("dark");
-buttonManager.register("link");
+new ButtonGenerator().registerAllAppearances();
